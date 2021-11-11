c0f6b11c894115998594593fea933fe1053e8e93
OmarOnntiveros <oontiveros@labsxd.com>
2021-11-10 22:34:39 -0300

    commit

--- Extra-Validacion-EntiempoReal.js
+++ Extra-Validacion-EntiempoReal.js
@@ -206,11 +206,3 @@ submitButtonActivation = () => {
         submitButton.toggleAttribute('disabled', false);
     }
 };
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    console.log([...formData]);
-    for (let [key, value] of formData.entries()) {
-        console.log(`${key}: ${value}`);
-    }
-});
